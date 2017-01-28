676c4bd3e6255ca3000948d4e104802a7266b8ea
Daksh Sharma <dashatrix@gmail.com>
2017-01-28 00:48:58 -0800

    [Android] - started android app, empty

---
+++ androidApp/GophertainmentAndroid/app/src/main/java/com/gophertainment/gophertainmentandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gophertainment.gophertainmentandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
