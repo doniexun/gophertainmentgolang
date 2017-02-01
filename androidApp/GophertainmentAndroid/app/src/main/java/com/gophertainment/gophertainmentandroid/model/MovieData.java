16f3c873ff291946c0a7d3d62e6ee1e6a3f2982f
Daksh Sharma <dashatrix@gmail.com>
2017-02-01 00:34:30 -0800

    [ANDROID] - added data model, card view, layout manager, adapter

---
+++ androidApp/GophertainmentAndroid/app/src/main/java/com/gophertainment/gophertainmentandroid/model/MovieData.java
@@ -0,0 +1,27 @@
+package com.gophertainment.gophertainmentandroid.model;
+
+/**
+ * Created by dakshsharma on 1/31/17.
+ */
+
+public class MovieData extends BaseModel {
+
+    private double
+            budget,
+            revenue,
+            voteAvg;
+
+    private int
+            voteCount,
+            runtime;
+
+    private String
+            tagline,
+            status,
+            originalLanguage,
+            releaseDate;
+
+    private String[] genre;
+
+
+}
---
+++ androidApp/GophertainmentAndroid/app/src/main/java/com/gophertainment/gophertainmentandroid/model/PersonData.java
@@ -0,0 +1,18 @@
+package com.gophertainment.gophertainmentandroid.model;
+
+/**
+ * Created by dakshsharma on 1/31/17.
+ */
+
+public class PersonData extends BaseModel {
+
+
+    private String birthday;
+    private String deathDay;
+    private String placeOfBirth;
+
+    private String[] imageProfiles;
+
+
+}
+
